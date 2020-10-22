62dbfced7d06f494588f5242e9e5a260e34e02bc
Belyaev126 <belyaevrw@gmail.com>
2020-10-22 23:49:28 +0300

    индивидуальное задание 2

---
+++ iz_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import math
+
+#Вариант 3
+#Из трех действительных чисел выбрать те, модули которых не меньше 4.
+
+if __name__ == '__main__':
+
+   a = int(input("Введите a: "))
+   b = int(input("Введите b: "))
+   c = int(input("Введите c: "))
+   if a>4:
+      print("{0}".format(a))
+   if b>4:
+      print("{0}".format(b))
+   if c>4:
+      print("{0}".format(c))
